8d6cc754e87408461f9f1921b79c3946869930aa
Silio <siliosilvestre@infoway-pi.com.br>
2020-04-02 09:05:55 -0300

    mostrando o período em abas

--- lib/screens/horarios/horarios_screen.dart
+++ lib/screens/horarios/horarios_screen.dart
@@ -5,57 +5,91 @@ import 'package:horariosonibusapp/screens/home/home_controller.dart';
 import 'package:horariosonibusapp/screens/horarios/horario_tile.dart';
 import 'package:horariosonibusapp/utils/utils.dart';
 
-class HorariosScreen extends StatelessWidget {
+class HorariosScreen extends StatefulWidget {
   final String codigoLinha;
   final String codigoParada;
-
-  final _homeController = GetIt.instance<HomeController>();
 
   HorariosScreen({Key key, this.codigoLinha, this.codigoParada})
       : super(key: key);
 
   @override
+  _HorariosScreenState createState() => _HorariosScreenState();
+}
+
+class _HorariosScreenState extends State<HorariosScreen>
+    with SingleTickerProviderStateMixin {
+  final _homeController = GetIt.instance<HomeController>();
+
+  TabController _tabController;
+
+  @override
   Widget build(BuildContext context) {
-    List paradas = _homeController.horarios[codigoLinha].keys.toList();
-    paradas.sort((a, b) => int.parse(a).compareTo(int.parse(b)));
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Horários de ${codigoLinha} em ${codigoParada}"),
-        actions: <Widget>[
-          Observer(
-            builder: (_) {
-              String mensagem = _homeController.horarios[codigoLinha]
-                          [codigoParada]['metricas']
-                      .replaceAll("       ", "\n")
-                      .replaceAll("      ", "\n") +
-                  "\n\nÚltima atualização: ${_homeController.horarios[codigoLinha][codigoParada]['ultima_atualizacao']}";
-              return IconButton(
-                icon: Icon(Icons.info_outline),
-                onPressed: () {
-                  dialogAlert(context,
-                      title: "Mais Informações", message: mensagem);
-                },
-              );
-            },
-          )
-        ],
-      ),
-      body: Observer(
-        builder: (_) {
-          String horarios =
-              _homeController.horarios[codigoLinha][codigoParada]['horarios'];
-          horarios = horarios.replaceAll("[", "");
-          horarios = horarios.replaceAll("]", "");
-          horarios = horarios.replaceAll("'", "");
-          horarios = horarios.replaceAll(" ", "");
+    return Observer(
+      builder: (_) {
+        List tabKeys = _homeController
+            .horarios[widget.codigoLinha][widget.codigoParada].keys
+            .toList();
 
-          List<String> listHorarios = horarios.split(",");
+        List<Tab> myTabs = <Tab>[];
+        for (String periodo in tabKeys) {
+          myTabs.add(Tab(text: periodo.toUpperCase()));
+        }
 
-          return ListView(
-            children: listHorarios.map((e) => HorarioTile(horario: e)).toList(),
-          );
-        },
-      ),
+        _tabController = TabController(vsync: this, length: tabKeys.length);
+
+        return Scaffold(
+            appBar: AppBar(
+              title: Text(
+                  "Horários de ${widget.codigoLinha} em ${widget.codigoParada}"),
+              actions: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.info),
+                  onPressed: () {
+                    String tab = tabKeys.elementAt(_tabController.index);
+                    String mensagem = _homeController
+                            .horarios[widget.codigoLinha][widget.codigoParada]
+                                [tab]['metricas']
+                            .replaceAll("       ", "\n")
+                            .replaceAll("      ", "\n") +
+                        "\n\nÚltima atualização: ${_homeController.horarios[widget.codigoLinha][widget.codigoParada][tab]['ultima_atualizacao']}";
+
+                    dialogAlert(context,
+                        title: "Mais Informações", message: mensagem);
+                  },
+                ),
+              ],
+              bottom: TabBar(
+                controller: _tabController,
+                tabs: myTabs,
+              ),
+            ),
+            body: TabBarView(
+              controller: _tabController,
+              children: myTabs.map((Tab tab) {
+                final String label = tab.text.toLowerCase();
+
+                String horarios = _homeController.horarios[widget.codigoLinha]
+                    [widget.codigoParada][label]['horarios'];
+                horarios = horarios.replaceAll("[", "");
+                horarios = horarios.replaceAll("]", "");
+                horarios = horarios.replaceAll("'", "");
+                horarios = horarios.replaceAll(" ", "");
+
+                List<String> listHorarios = horarios.split(",");
+
+                return ListView(
+                  children:
+                      listHorarios.map((e) => HorarioTile(horario: e)).toList(),
+                );
+              }).toList(),
+            ));
+      },
     );
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
   }
 }
